4bbaf7ee5e49d89aec7963698dc330991541722f
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2025-06-27 11:58:52 -0300

    Gestion del editor

--- italiano-app/src/Controller/editor.controller.js
+++ italiano-app/src/Controller/editor.controller.js
@@ -2,9 +2,15 @@ import verbModel from "../Model/verbos.model.js";
 
 export const editorController = async(req,res) => {
     try {
-        const {verbo_it,verbo_trd} = req.body;
-        const newVerb = await verbModel.create({verbo_it,verbo_trd})
-        res.status(201).json({message:"added",payload:req.body});
+        const check = await verbModel.findOne({verbo_it:req.body.verbo_it});
+        console.log(check);
+        if(check){
+            res.status(409).json({message:"Verbo già creato"})
+        }else{
+            const newVerb = await verbModel.create(req.body)
+            res.status(201).send(newVerb);
+        }
+        
     } catch (error) {
         res.status(500).json({message:"Server connection error"})
     }
